7c81545496608015d8b4bdd487d5cb59945ca104
Yannick Müller <mail@ynnk.dev>
2024-12-16 08:31:04 +0100

    fix: Resolved issues from pr

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="h-full w-full flex justify-center items-center bg-[#141e36] flex-col">
-        <h1 class="text-5xl bg-transparent text-white mb-8">Dashboard</h1>
-        <h2 class="text-3xl text-white italic mb-4">Total amount: {{ totalAmount }}</h2>
-        <h3 class="text-2xl text-white italic">Transaktonen</h3>
+    <div class="h-full w-full flex justify-center items-center bg-black flex-col">
+        <h1 class="text-5xl bg-transparent mb-8">Insimodus</h1>
+        <h2 class="text-3xl italic mb-4">Total amount: {{ totalAmount }}</h2>
+        <h3 class="text-2xl  italic">Transaktionen</h3>
         <div class="h-3/4 py-12 w-1/2">
-            <div v-for="transaction in transactions" class="p-4 flex justify-between w-full" v-bind:key="transaction.id">
-                <span class="text-white">{{ transaction.title }}</span>
-                <span class="text-white">{{ transaction.positiv ? "+" : "-" }}{{ transaction.amount }}</span>
+            <div v-for="transaction in transactions" class="p-4 flex justify-between w-full"
+                v-bind:key="transaction.id">
+                <span class="">{{ transaction.title }}</span>
+                <span class="">{{ transaction.positiv ? "+" : "-" }}{{ transaction.amount }}</span>
             </div>
-            <div class="mt-4 flex justify-center items-center w-full h-10 border rounded-lg border-dashed cursor-pointer" @click="addTransaction">
-                <span class="text-white text-2xl">+</span>
+            <div class="mt-4 flex justify-center items-center w-full h-10 border rounded-lg border-dashed cursor-pointer"
+                @click="addTransaction">
+                <span class=" text-2xl">+</span>
             </div>
         </div>
     </div>
@@ -20,7 +22,7 @@ import { getTransactions } from '@/service/data';
 
 export default {
     name: "InsimodusDashboard",
-    data(){
+    data() {
         return {
             transactions: [] as Transaction[]
         }
@@ -29,7 +31,7 @@ export default {
         this.transactions = getTransactions();
     },
     computed: {
-        totalAmount(){
+        totalAmount() {
             let sum = 0;
             this.transactions.map(transaction => sum += transaction.positiv ? transaction.amount : -transaction.amount)
 
@@ -37,7 +39,7 @@ export default {
         }
     },
     methods: {
-        addTransaction(){
+        addTransaction() {
             window.location.href = "/add";
         }
     }
