af47a190b51608f4d9cb5def2bf97ee2f1f8403e
Yannick Müller <mail@ynnk.dev>
2024-12-16 08:14:23 +0100

    feat: Add datamodel and data persistence layer #20 (#46)
    * feat: Add datamodel and data persistence layer #20
    
    * fix: Fix logic bug in get and save endpoint
    
    * fix: Fix ESLint issues

---
+++ src/models/transaction.ts
@@ -0,0 +1,6 @@
+export interface Transaction {
+    positiv: boolean
+    amount: number
+    title: string
+    id: number | undefined
+}
---
+++ src/service/data.ts
@@ -0,0 +1,35 @@
+import type { Transaction } from "@/models/transaction";
+
+export function saveTransaction(transaction: Transaction): number {
+    const transactions = JSON.parse(localStorage.getItem("transactions") || "{}")
+
+    let maxIndex = 0;
+
+    Object.keys(transactions).map(key => {
+        if (+key > maxIndex) {
+            maxIndex = +key;
+        }
+    });
+
+    console.log(maxIndex)
+    transaction.id = maxIndex + 1;
+
+    transactions[transaction.id.toString()] = transaction;
+
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+
+    return transaction.id;
+}
+
+export function deleteTransaction(id: number) {
+    const transactions = JSON.parse(localStorage.getItem("transactions") || "{}")
+
+    delete transactions[id]
+
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+export function getTransactions(): Array<Transaction> {
+    return Object.values(JSON.parse(localStorage.getItem("transactions") || "{}"))
+}
+
